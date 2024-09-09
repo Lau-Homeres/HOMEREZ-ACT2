0fc498d6e286a7ed1e3ff48875e88d6fa8f0ba32
Lau Homeres <laurencehomeres0@gmail.com>
2024-09-09 22:06:09 +0800

    initial commit

---
+++ Variables.js
@@ -0,0 +1,8 @@
+let name = "LAU";
+let whatDoYouWannaBecomeInYourLife = "A programmer";
+let gender = "Male";
+let twitterHandle = "its_rence_life";
+console.log(name);
+console.log(whatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(twitterHandle);
